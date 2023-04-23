ffea14a76e9eb52874bb063275de655dcf884971
Guy <gallgood@csumb.edu>
2023-04-22 20:56:12 -0700

    Down to the TreeView

--- src/FormDesignPrompt.java
+++ src/FormDesignPrompt.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class formDesignPrompt {
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JTree;
+
+public class FormDesignPrompt extends JFrame{
+  private JPanel formDesignPrompt;
+  private JTextField txtNets;
+  private JLabel lblNets;
+  private JTextField txtIP;
+  private JLabel lblNodes;
+  private JTextField txtNodes;
+  private JTextField txtConnections;
+  private JLabel lblConnections;
+  private JComboBox comboBox1;
+  private JTree treeNetworks;
+  private JButton previousButton;
+  private JButton calculateButton;
+  private static FormMain formMainReference;
+
+  FormDesignPrompt(String title) {
+    super(title);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setContentPane(formDesignPrompt);
+    this.pack();
+    this.setLocationRelativeTo(null);
+    this.setVisible(true);
+
+    treeNetworks
+  }
+
 }
--- src/FormSubnetPrompt.java
+++ src/FormSubnetPrompt.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class formSubnetPrompt {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FormSubnetPrompt extends JFrame {
+  private JPanel formSubnetPrompt;
+
+  FormSubnetPrompt(String title){
+    super(title);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setContentPane(formSubnetPrompt);
+    this.pack();
+    this.pack();
+    this.setResizable(false);
+    this.setLocationRelativeTo(null);
+    this.setVisible(true);
+  }
 }
